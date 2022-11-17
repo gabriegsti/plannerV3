fed5dbdc9ba1416baf36169ec8b1b49f42c7147d
gabriegsti <gabrielgsdevbr@gmail.com>
2022-11-17 16:41:28 -0300

    removendo aula do dto

--- Planner.Dados/DTO/Anotacao/UpdateAnotacaoDTO.cs
+++ Planner.Dados/DTO/Anotacao/UpdateAnotacaoDTO.cs
@@ -10,7 +10,6 @@ namespace Planner.Dados.DTO.Anotacao
     public class UpdateAnotacaoDTO
     {
         public int Id_Anotacao { get; set; }
-        public int? AulaId { get; set; }
         public string? Titulo { get; set; }
         public string? Campo_Texto { get; set; }
         public string? Link { get; set; }
